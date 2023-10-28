878e15a013431fd3ae31b852d0e2a5f48e77872f
marinica dragoslav <marinicadragoslav@gmail.com>
2023-10-28 20:24:49 +0300

    Add info about allocation to memory segments

--- project/src/main.c
+++ project/src/main.c
@@ -1,11 +1,26 @@
+/*
+memory allocation, global:
+
+const, initialized      => .rodata
+const, not initialized  => .rodata
+var, initialized        => .data
+var, not initialized    => .bss
+*/
 const int gci = 5;              /* .rodata */
 const int gcu;                  /* .rodata */
-
 int gvi = 10;                   /* .data */
 int gvu;                        /* .bss */
 
 int main(void)
 {
+    /*
+    memory allocation, static:
+
+    static const, initialized      => .rodata
+    static const, not initialized  => .rodata
+    static var, initialized        => .data
+    statuc var, not initialized    => .bss
+    */
     static const int sci = 2;   /* .rodata */
     static const int sc;        /* .rodata */
     static int svi = 3;         /* .data */
